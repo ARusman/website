4919f6721fc7cb0d7f125819d23e14fcd95c09a7
ARusman <andrea@trueprice.org>
2016-12-07 19:27:41 +0100

    newcommit

--- co2/views.py
+++ co2/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
-from . models import Capital
+from .models import Capital
 
 def index(request):
     all_capitals = Capital.objects.all()
-    context = {'all_capitals': all_capitals}
-    return render(request, 'co2/index.html', context)
+    return render(request, 'co2/index.html', {'all_capitals': all_capitals})
 
 def detail(request, capital_id):
-    return HttpResponse("<h2>Details for Capital id: " + str(capital_id) + "</h2>")
+    try:
+        capital = Capital.objects.get(pk=capital_id)
+    except Capital.DoesNotExist:
+        raise Http404("This Capital does not exist")
+    return render(request, 'co2/detail.html', {'capital': capital})
