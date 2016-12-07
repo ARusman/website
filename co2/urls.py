7b70bab60ea4c615d8e3f648fb3515a21194e541
ARusman <andrea@trueprice.org>
2016-12-07 17:49:51 +0100

    newcommit

--- co2/urls.py
+++ co2/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /co2/
     url(r'^$', views.index, name='index'),
+
+    # /co2/<the id>/
+    url(r'^(?P<capital_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- co2/views.py
+++ co2/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import Capital
 
 def index(request):
-    return HttpResponse("<h1>This is the CO2 valuation page</h1>")
+    all_capitals = Capital.objects.all()
+    template = loader.get_template('co2/index.html')
+    context = {
+        'all_capitals': all_capitals,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, capital_id):
+    return HttpResponse("<h2>Details for Capital id: " + str(capital_id) + "</h2>")
